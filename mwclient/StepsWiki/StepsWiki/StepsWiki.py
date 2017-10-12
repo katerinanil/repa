1ab3dbf35181ca552685d3cdeeb7543d536ae528
gologames@bk.ru <1qa2ws3ed>
2017-10-13 00:35:09 +0300

    Updated StepsWiki.py

---
+++ mwclient/StepsWiki/StepsWiki/StepsWiki.py
@@ -0,0 +1,16 @@
+﻿import mwclient
+
+categories = ['Отглагольные существительные на -ка',
+			  'Отглагольные существительные на -ание',
+			  'Отглагольные существительные на -ение‎']
+site = mwclient.Site('ru.wiktionary.org')
+for c in categories:
+	for page in site.Categories[c]:
+		if page.page_title != 'компилирование': continue
+		for l in page.text().split():
+			if l[:7] == '|основа':
+				print("L='''", l.replace('\u0301', ''), "'''", sep='')
+				stI = l.find('=', 7)
+				stem = l[stI+1:].replace('\u0301', '')
+				# if len(stem):
+				# 	print(page.page_title, '-', stem)
---
+++ mwclient/makeStemsWiki.py
@@ -0,0 +1,18 @@
+import mwclient
+
+f = open('stems.txt', 'w')
+categories = ['Отглагольные существительные на -ка',
+			  'Отглагольные существительные на -ание',
+			  'Отглагольные существительные на -ение‎']
+site = mwclient.Site('ru.wiktionary.org')
+for c in categories:
+	for page in site.Categories[c]:
+		for l in page.text().split():
+			if l[:7] == '|основа':
+				stI = l.find('=', 7)
+				if stI == -1: continue
+				stem = l[stI+1:].replace('\u0301', '')
+				stem = stem.replace('\u045d', '')
+				if len(stem):
+					print(page.page_title, '-', stem)
+					f.write(stem + ', ')
