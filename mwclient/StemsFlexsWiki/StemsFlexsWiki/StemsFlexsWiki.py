85d1b467320eb3787166e3142aeb2c68fddeacbd
gologames@bk.ru <1qa2ws3ed>
2017-10-16 23:12:40 +0300

    Added StemsFlexsWiki proj

---
+++ mwclient/StemsFlexsWiki/StemsFlexsWiki/StemsFlexsWiki.py
@@ -0,0 +1,36 @@
+﻿from pprint import pprint
+import mwclient
+
+site = mwclient.Site('ru.wiktionary.org')
+categories = ['Отглагольные существительные на -ка',
+			  'Отглагольные существительные на -ание',
+			  'Отглагольные существительные на -ение‎'
+              ]
+stems = {}
+templs = set()
+
+for c in categories:
+    for page in site.Categories[c]:
+        isStem = False
+        templ = ''  
+        for l in page.text().split('\n'):
+            if not isStem:
+                if l[:8] == '{{сущ ru':
+                    isStem = True
+                    templ = l[2:]
+            else:
+                if l[:7] == '|основа':
+                    stI = l.find('=', 7)
+                    if stI == -1: continue
+                    stem = l[stI+1:].replace('\u0301', '')
+                    stem = stem.replace('\u045d', '')
+                    stemNum = l[1:stI]
+                    if not len(stem): continue
+                    stems.setdefault(stem, set()).add((templ, stemNum))
+                else:
+                    isStem = False
+                    templs.add(templ)
+
+pprint(stems)
+print()
+pprint(templs)
