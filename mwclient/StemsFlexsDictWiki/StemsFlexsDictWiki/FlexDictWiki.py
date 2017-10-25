d20fe0faf665866454f04149a979fe87526dc1cd
katerinanil <katerinanil16@gmail.com>
2017-10-25 22:27:28 +0300

    FlexDictWiki.py

--- mwclient/StemsFlexsDictWiki/StemsFlexsDictWiki/FlexDictWiki.py
+++ mwclient/StemsFlexsDictWiki/StemsFlexsDictWiki/FlexDictWiki.py
@@ -7,26 +7,30 @@ site = mwclient.Site('ru.wiktionary.org')
 templDict = {}
 templArr = []
 
+print(templs)
+
 for t in templs:
     tem = 'Шаблон:%s' % t
     templArr.append(tem)
-    #return (templArr)
-    print(templArr)
-    for temp in templArr:
-        page = site.Pages[temp]
-        for p in page:
-            temp = temp[7:]
-            isFlex = False
-            for l in page.text().split('\n'):
-                if not isFlex:
-                    if l[8:17] == '={{{основа':
-                        isFlex = True
-                        stNumStart = l.find('}', 12)
-                        stNumEnd = l.find('о', 10)
-                        stemNum = l[stNumStart:stNumEnd]
-                        flexStart = l.find('}', 12)
-                        flex = l[flexStart+2:]
-                        templDict.setdefault(flex, set()).add((temp, stemNum))
+    print(tem)
+print(templArr)
+for temp in templArr:    
+    page = site.Pages[temp]
+    temp = temp[7:]
+    isFlex = False
+    for l in page.text().split('\n'):
+        #print (l[:2])
+        #if not isFlex:
+        if l[8:17] == '={{{основа':
+            isFlex = True
+            stNumStart = l.find('}', 12)
+            stNumEnd = l.find('о', 10)
+            stemNum = l[stNumStart:stNumEnd]
+            print(stemNum)
+            flexStart = l.find('}', 12)
+            flex = l[flexStart+2:]
+            print(flex)
+            templDict.setdefault(flex, set()).add((temp, stemNum))
 
 if __name__ == '__main__':
     pprint(templDict)
