c21999b15d1f841a40a50c69be0cb4b09d921a58
katerinanil <katerinanil16@gmail.com>
2017-11-30 22:03:01 +0300

    aho_cor.py

--- web_server/aho.py
+++ web_server/aho.py
@@ -1,9 +1,10 @@
-from string_algorithm import aho_corrasick
+#from string_algorithm import aho_corrasick
+from aho_cor import aho_corrasick
+from collections import deque
+from .trie import Trie, TrieNode
 
-morphArr = ['мам','ами','а','ми','ам','и']
+morphArr = ('мам','ами','а','ми','ам','и')
 
 def getMorph(query):
     morphemes = aho_corrasick.find(morphArr, query)
     return(morphemes)
-
-def
