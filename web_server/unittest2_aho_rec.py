a07aad39b575565b3be9bd400671f2fda64aabf2
gologames@bk.ru <1qa2ws3ed>
2017-12-11 22:02:53 +0300

    Added unittest2_aho_rec

--- web_server/unittest2_aho_rec.py
+++ web_server/unittest2_aho_rec.py
@@ -1,2 +1,11 @@
-#import unittest
-#import 
+import unittest
+from aho_rec import aho
+
+class AhoCase(unittest.TestCase):
+    def test_aho0(self):
+        ass = [[(0, 'a'), (1, 'b')], [(0, 'ab')]]
+        res = sorted(list(aho(('a', 'b', 'ab'), 'ab')))
+        self.assertEqual(ass, res)
+
+if __name__ == '__main__':
+    unittest.main()
