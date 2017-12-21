3f05e95e24c70b3b66297a67b1c625160ec1dc13
katerinanil <katerinanil16@gmail.com>
2017-12-21 23:27:47 +0300

    jic

--- web_server/state_machine.py
+++ web_server/state_machine.py
@@ -34,9 +34,8 @@ class MorphSM:
             else: return False
         return True
 
-morphs = { 'по' : {MorphSM.Pr, MorphSM.R}, 'на' : {MorphSM.Pr}, 'а' : {MorphSM.So, MorphSM.F},
-           'смотр' : {MorphSM.R}, 'е' : {MorphSM.So, MorphSM.F}, 'вш' : {MorphSM.R, MorphSM.Si},
-           'ий' : {MorphSM.F}, 'ся' : {MorphSM.Ps} }
+morphs = { 'князь' : {MorphSM.R}, 'княз' : {MorphSM.R},'я' : {MorphSM.F},'ю' : {MorphSM.F},'ями' : {MorphSM.F},'под' : {Morph.Pr},'при' : {Morph.Pr},'ех' : {MorphSM.R},'а' : {MorphSM.So, MorphSM.F},'л' : {MorphSM.So}, 'по' : {MorphSM.Pr, MorphSM.R}, 'на' : {MorphSM.Pr}, 'а' : {MorphSM.So, MorphSM.F},
+           'смотр' : {MorphSM.R}, 'е' : {MorphSM.So, MorphSM.F}, 'вш' : {MorphSM.R, MorphSM.Si},'ий' : {MorphSM.F}, 'ся' : {MorphSM.Ps},'на' : {MorphSM.R},'по' : {MorphSM.R},'над' : {MorphSM.R},'в' : {MorphSM.R},'ех' : {MorphSM.R},'из' : {MorphSM.R},'под' : {MorphSM.R} }
 
 def getCombo(word, morphs):
     #ans - combo
