a7cc1e0228c349eed616c3e0298d4c4caad33154
gologames@bk.ru <1qa2ws3ed>
2018-04-04 00:36:26 +0300

    Some work for understanding how to bind tkinker to Chaplip

--- chatbot/CinemaHall/tk_cinema_hall.py
+++ chatbot/CinemaHall/tk_cinema_hall.py
@@ -1,18 +1,12 @@
 from tkinter import *
-#from Pillow import ImageTk, Image
 
 root = Tk()
 root.title(u'CHAPLIN')
-#root.geometry('381x425')
 root.geometry('745x525')
 root.resizable(width=False, height=False)
 root.tk.call('wm', 'iconphoto', root._w, PhotoImage(file='c2.png'))
 frame = Frame(bg = 'black')
 
-seat_name = 1
-row_name = 10
-
-#img = imageTk.PhotoImage(Image.open('ico.png'))
 chap1 = PhotoImage(file="ico.png")
 Label(root, image = chap1).grid(row=0, column=0, columnspan = 4)
 chap2 = PhotoImage(file="ico.png")
@@ -23,9 +17,7 @@ Label(root, image = chap3).grid(row=0, column=4, columnspan = 4)
 li = ["grey", "royalblue"]
 def color(event):
      event.widget.configure(bg=li[0])
-     li[0],li[1] = li[1],li[0]
-     #li[0] = li[1]
-     #li[1] = li[0]
+     li[0], li[1] = li[1], li[0]
 
 Button(root, text = "Buy",  background="light sea green",
                foreground="white",).grid(row = 0, column = 6, columnspan = 2, ipadx = 10, padx = 3, pady = 5, sticky = E)
@@ -34,45 +26,20 @@ Button(root, text = "Book",  background="royalblue",
 Button(root, text = "Мне повезет!",  background="coral",
                foreground="white",).grid(row = 0, column = 9, columnspan = 2, ipadx = 10, padx = 3, pady = 5, sticky = E)
 
-
+row_name = 10
 for x in range(5, 15):
+    label1 = Label(root, text = "ряд "+str(row_name)).grid(row = x, column = 0)
+    label2 = Label(root, text = "ряд "+str(row_name)).grid(row = x, column = 12)
+    seat_name = 1
     for z in range(1, 11):
-        label1 = Label( root, text = "ряд "+str(row_name)).grid(row = x, column = 0)
         but2 = Button(root, text = str(seat_name) + " (250р)",  background="royalblue", foreground="white")
         but2.grid(row = x, column = z, ipadx = 4, ipady = 2, padx = 2, pady = 2)
         but2.bind("<Button-1>", color)
-        label2 = Label( root, text = "ряд "+str(row_name)).grid(row = x, column = 12)
         seat_name += 1
-        if seat_name == 11:
-            seat_name = 1
     row_name -= 1
-    
 
-class Example(Frame):
-     def __init__(self, parent):
-          Frame.__init__(self, parent)   
-          self.parent = parent        
-          self.initUI()
-        
-     def initUI(self):
-          self.parent.title("Shapes")        
-          self.pack(fill=BOTH, expand=1)
- 
-          canvas = Canvas(self)
-          canvas.create_rectangle(305, 35, 405, 45, outline="blue", fill="red", width=2)
-          canvas.pack(fill=BOTH, expand=1)
-
-but = Button(root, text = "Э К Р А Н",  bg="indigo",
-       foreground="white",)
+but = Button(root, text = "Э К Р А Н",  bg="indigo", foreground="white",)
 but.grid(columnspan = 16, padx = 18, ipady = 5, pady = 15, sticky = W+E)
 but.bind("<Button-1>", color)
 
 root.mainloop()
-
-"""import Tkinter
-root = Tkinter.Tk(  )
-for r in range(3):
-   for c in range(4):
-      Tkinter.Label(root, text='R%s/C%s'%(r,c),
-         borderwidth=1 ).grid(row=r,column=c)
-root.mainloop(  )"""
